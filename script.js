2f65b025789d9ad3ba396e847b25c2ed53dbd24d
Ailish <snickrrdoodles@gmail.com>
2024-10-22 09:24:34 -0400

    Disallowed empty inputs and added container background

--- script.js
+++ script.js
@@ -7,12 +7,17 @@ function strikethrough(element){
     }
 }
 
-function addItem(){    
-    let x = document.getElementById("input").value
-    let item = `<li onclick="strikethrough(this)">${x}</li>`
-    let li = document.createElement("li")
-    li.innerHTML = item
-    document.getElementById("listSet").appendChild(li)
+function addItem(){
+    if(document.getElementById("input").value === ''){
+        alert("Invalid input. Your item must have a name.")
+    }
+    else{
+        let x = document.getElementById("input").value
+        let item = `<li onclick="strikethrough(this)">${x}</li>`
+        let li = document.createElement("li")
+        li.innerHTML = item
+        document.getElementById("listSet").appendChild(li)
 
-    document.getElementById("input").value=''
+        document.getElementById("input").value=''
+    } 
 }
